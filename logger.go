7b858fb7df03e5f384ba8c15c309d582340254a9
Crusher131 <jefe.zaca@icloud.com>
2025-05-09 10:01:20 -0300

    Fix If file not writable

--- logger.go
+++ logger.go
@@ -37,7 +37,7 @@ func Init(f ...cfgopt) {
 	if c.writeTerminal && c.writeFile {
 		err := createLogFile(&c)
 		if err != nil {
-			_cfg.fatalErrorConsole(fmt.Sprintln(err))
+			Fatal(err)
 			os.Exit(1)
 		}
 		c.multiWriter = io.MultiWriter(c.file, os.Stdout)
